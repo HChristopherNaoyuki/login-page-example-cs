ce9303300ae2b7d2a69fdfbab42e0b138e47a101
HChristopherNaoyuki <st10462415@rcconnect.edu.za>
2025-05-26 14:22:30 +0200

    Add project files.

---
+++ login-page-example-cs/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace login_page_example_cs;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void open_login(object sender, RoutedEventArgs e)
+    {
+        /**
+         * Change page.
+         * Set the login: Hidden.
+         */
+
+        login_page.Visibility = Visibility.Hidden;
+
+        /**
+         * Set the chat page on Visible.
+         */
+
+        chats_page.Visibility = Visibility.Visible;
+    }
+
+    private void back(object sender, RoutedEventArgs e)
+    {
+        
+
+        login_page.Visibility = Visibility.Hidden;
+
+        // Set the chat page on Visible.
+        chats_page.Visibility = Visibility.Visible;
+    }
+}
